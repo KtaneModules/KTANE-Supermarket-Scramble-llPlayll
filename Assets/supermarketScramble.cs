a8a19f187b7aeed0d9b00b9dc4e424e688d0718a
llPlayll <129607915+llPlayll@users.noreply.github.com>
2024-02-18 04:10:48 +0500

    Module done... kinda

--- Assets/supermarketScramble.cs
+++ Assets/supermarketScramble.cs
@@ -14,13 +14,21 @@ public class supermarketScramble : MonoBehaviour
     [SerializeField] private AudioSource AudioSrc;
 
     [SerializeField] private AudioClip SupermarketMusic;
-    [SerializeField] private List<TextMesh> ItemTexts;
+    [SerializeField] private List<GameObject> ItemTexts;
     [SerializeField] private GameObject ListObject;
     [SerializeField] private GameObject AislesParent;
     [SerializeField] private KMSelectable ListToggle;
+    [SerializeField] private AudioClip ToggleClip;
     [SerializeField] private List<GameObject> ItemButtons;
     [SerializeField] private List<KMSelectable> AisleArrows;
     [SerializeField] private List<GameObject> Aisles;
+    [SerializeField] private TextMesh HighlightText;
+    [SerializeField] private TextMesh AisleNumberText;
+    [SerializeField] private List<KMSelectable> SlotButtons;
+    [SerializeField] private AudioClip SuccessClip;
+    [SerializeField] private GameObject CheckoutAisle;
+    [SerializeField] private GameObject SolveButton;
+    [SerializeField] private TextMesh SolveText;
 
     List<string> items = new List<string>() { "CUCUMBER", "CELERY", "TOMATO", "SPINACH", "BROCCOLI", "LETTUCE", "ZUCCHINI", "ASPARAGUS", "GARLIC", "CARROT", "ONION", "POTATOES", "CHICKEN", "SAUSAGES", "PORKCHOP", "STEAK", "TURKEY", "HOTDOGS", "BACON", "SALMON", "SHRIMP", "SUSHI", "MUFFINS", "CUPCAKES", "DONUTS", "COOKIES", "DANISHES", "BAGUETTE", "TORTILLAS", "BAGELS", "BREAD", "CROISSANTS", "BANANA", "PINEAPPLE", "COCONUT", "AVOCADO", "LEMON", "ORANGE", "CHERRIES", "GRAPES", "SOUP", "TUNA", "CEREAL", "CRACKERS", "PEANUTS", "COCOA", "MOLASSES", "CINNAMON", "OATMEAL", "SUGAR", "COFFEE", "FLOUR", "BATTERIES", "RAZORS", "DEODORANT", "SHAMPOO", "BLEACH", "SPONGES", "DETERGENT", "NAPKINS", "DIAPERS", "TISSUES", "POPCORN", "CHOCOLATE", "PIZZA", "ICE CREAM", "POPSICLES", "MILK", "JUICE", "CHEESE", "YOGURT", "EGGS", "MARGARINE", "BUTTER", "RICE", "HONEY", "PASTA", "JELLY", "MUSTARD", "VINEGAR" };
 
@@ -31,6 +39,10 @@ public class supermarketScramble : MonoBehaviour
     bool listView;
     string selectedItem;
     int curAisle;
+    string[] slottedItems = new string[8];
+    int correctCount;
+    bool success;
+    int hours, minutes, seconds = 0;
 
     static int ModuleIdCounter = 1;
     int ModuleId;
@@ -44,17 +56,19 @@ public class supermarketScramble : MonoBehaviour
         ListToggle.OnInteract += delegate () { ToggleList(); return false; };
         foreach (GameObject button in ItemButtons) {
             button.GetComponent<KMSelectable>().OnInteract += delegate () { ItemPress(button); return false; };
+            button.GetComponent<KMSelectable>().OnSelect += delegate () { HighlightText.text = button.GetComponent<MarketItem>().ItemName == "ICECREAM" ? "ICE CREAM" : button.GetComponent<MarketItem>().ItemName; };
+            button.GetComponent<KMSelectable>().OnDeselect += delegate () { HighlightText.text = ""; };
+        }
+        foreach (KMSelectable slot in SlotButtons) {
+            slot.OnInteract += delegate () { SlotPress(SlotButtons.IndexOf(slot)); return false; };
+            slot.OnSelect += delegate () { HighlightText.text = slottedItems[SlotButtons.IndexOf(slot)]; };
+            slot.OnDeselect += delegate () { HighlightText.text = ""; };
         }
         foreach (KMSelectable arrow in AisleArrows) {
             arrow.OnInteract += delegate () { CycleAisles(AisleArrows.IndexOf(arrow)); return false; };
         }
-        /*
-        foreach (KMSelectable object in keypad) {
-            object.OnInteract += delegate () { keypadPress(object); return false; };
-            }
-        */
 
-        //button.OnInteract += delegate () { buttonPress(); return false; };
+        SolveButton.GetComponent<KMSelectable>().OnInteract += delegate () { Solved(); return false; };
     }
 
     void ModuleSelected()
@@ -62,11 +76,15 @@ public class supermarketScramble : MonoBehaviour
         if (!moduleStarted)
         {
             moduleStarted = true;
-            AudioSrc.clip = SupermarketMusic; 
+            AudioSrc.clip = SupermarketMusic;
             AudioSrc.Play();
 
             listView = true;
             ListObject.SetActive(true);
+            foreach (GameObject iText in ItemTexts)
+            {
+                iText.SetActive(true);
+            }
             AislesParent.SetActive(false);
             for (int i = 0; i < Aisles.Count; i++)
             {
@@ -81,6 +99,9 @@ public class supermarketScramble : MonoBehaviour
 
     void ToggleList()
     {
+        ListToggle.AddInteractionPunch();
+        Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, ListToggle.transform);
+        Audio.PlaySoundAtTransform(ToggleClip.name, transform);
         listView = !listView;
         ListObject.SetActive(listView);
         AislesParent.SetActive(!listView);
@@ -88,16 +109,86 @@ public class supermarketScramble : MonoBehaviour
 
     void ItemPress(GameObject itemObj)
     {
+        itemObj.GetComponent<KMSelectable>().AddInteractionPunch();
+        Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, itemObj.transform);
         selectedItem = itemObj.GetComponent<MarketItem>().ItemName;
+    }
+
+    void SlotPress(int slotIdx)
+    {
+        SlotButtons[slotIdx].GetComponent<KMSelectable>().AddInteractionPunch();
+        Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, SlotButtons[slotIdx].transform);
+        if (selectedItem != "" & !success)
+        {
+            bool itemWasCorrect = slottedItems[slotIdx] == modUnscrItems[slotIdx];
+
+            slottedItems[slotIdx] = selectedItem == "ICECREAM" ? "ICE CREAM" : selectedItem;
+            HighlightText.text = selectedItem;
+
+            bool itemCorrect = slottedItems[slotIdx] == modUnscrItems[slotIdx];
+            if (!itemWasCorrect)
+            {
+                correctCount += itemCorrect ? 1 : 0;
+            }
+            else if (!itemCorrect)
+            {
+                correctCount -= 1;
+            }
+            Log($"Put {(selectedItem == "ICECREAM" ? "ICE CREAM" : TitleString(selectedItem))} in slot {slotIdx + 1}. That is {(itemCorrect ? "correct" : "incorrect")}. {correctCount}/8 correct items.");
+            success = correctCount == 8;
+            if (success)
+            {
+                Log("All items collected. Checkout is permanently open!");
+                Audio.PlaySoundAtTransform(SuccessClip.name, transform);
+                SolveButton.SetActive(true);
+            }
+        }
     }
 
     void CycleAisles(int dir)
     {
+        Audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, AisleArrows[dir].transform);
         curAisle += dir * 2 - 1;
+        if (curAisle == Aisles.Count)
+        {
+            curAisle = Aisles.Count - 1;
+        }
+        else if (curAisle == -2)
+        {
+            curAisle = -1;
+        }
+
+        AisleNumberText.text = curAisle == -1 ? "C" : (curAisle + 1).ToString();
+        AisleNumberText.fontSize = curAisle > 8 ? 105 : 200;
         for (int i = 0; i < Aisles.Count; i++)
         {
             Aisles[i].SetActive(i == curAisle);
         }
+        CheckoutAisle.SetActive(curAisle == -1);
+    }
+
+    void Solved()
+    {
+        GetComponent<KMBombModule>().HandlePass();
+        ModuleSolved = true;
+
+        ListObject.SetActive(false);
+        AislesParent.SetActive(false);
+        ListToggle.gameObject.SetActive(false);
+        AudioSrc.Stop();
+        DisplayTimer();
+    }
+
+    void DisplayTimer()
+    {
+        SolveText.gameObject.SetActive(true);
+        string timerText = "";
+        if (hours > 0)
+        {
+            timerText += hours + ":";
+        }
+        timerText += $"{(minutes < 10 ? "0" + minutes.ToString() : minutes.ToString())}:{(seconds < 10 ? "0" + seconds.ToString() : seconds.ToString())}";
+        SolveText.text = $"YOU'VE FINISHED 1ST!\nYOUR TIME:\n{timerText}";
     }
 
     void GenItems()
@@ -120,7 +211,7 @@ public class supermarketScramble : MonoBehaviour
         for (int i = 0; i < 8; i++)
         {
             modScrItems.Add(ScrambleString(modUnscrItems[i]));
-            ItemTexts[i].text = $"{i + 1}. {modScrItems[i]}";
+            ItemTexts[i].GetComponent<TextMesh>().text = $"{i + 1}. {modScrItems[i]}";
             logScrambled += modScrItems[i];
             logScrambled += (i == 8 ? "." : ", ");
         }
@@ -129,7 +220,14 @@ public class supermarketScramble : MonoBehaviour
 
     void Start()
     {
+        ListObject.SetActive(true);
         AislesParent.SetActive(false);
+        SolveButton.SetActive(false);
+        foreach (GameObject iText in ItemTexts)
+        {
+            iText.SetActive(false);
+        }
+        StartCoroutine("Timer");
     }
 
     string TitleString(string n)
@@ -153,6 +251,25 @@ public class supermarketScramble : MonoBehaviour
     void Log(string arg)
     {
         Debug.Log($"[Supermarket Scramble #{ModuleId}] {arg}");
+    }
+
+    IEnumerator Timer()
+    {
+        while (!ModuleSolved)
+        {
+            yield return new WaitForSeconds(1);
+            seconds += 1;
+            if (seconds == 60)
+            {
+                minutes += 1;
+                seconds = 0;
+                if (minutes == 60)
+                {
+                    hours += 1;
+                    minutes = 0;
+                }
+            }
+        }
     }
 
 #pragma warning disable 414
